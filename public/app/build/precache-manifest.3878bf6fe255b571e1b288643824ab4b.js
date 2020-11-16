5ed1d899297599e92eb1067e86a4d844eda8b34b
Darlan Diogo <darlandiogo@hotmail.com>
2020-11-16 11:44:44 -0300

    .

---
+++ public/app/build/precache-manifest.3878bf6fe255b571e1b288643824ab4b.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "df780a19581592398bc9ad00c30e4885",
+    "url": "/index.html"
+  },
+  {
+    "revision": "c434b90733eaffe2b667",
+    "url": "/static/css/main.a7d1df0f.chunk.css"
+  },
+  {
+    "revision": "36d3c93977aa7dc029b6",
+    "url": "/static/js/2.ad0f0907.chunk.js"
+  },
+  {
+    "revision": "3da521dbb4874350fb1d5ccbd1bab881",
+    "url": "/static/js/2.ad0f0907.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "c434b90733eaffe2b667",
+    "url": "/static/js/main.abb0de49.chunk.js"
+  },
+  {
+    "revision": "4d7aff356d3d0d143cc8",
+    "url": "/static/js/runtime-main.fe22deb3.js"
+  }
+]);
