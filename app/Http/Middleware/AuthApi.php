7dfdcb393f767d6b8106227d3e3c7256b47ea64a
@darlan.fonseca <darlan.fonseca@csptecnologia.com>
2023-08-20 23:39:14 -0300

    ajuste em middleware auth

--- app/Http/Middleware/AuthApi.php
+++ app/Http/Middleware/AuthApi.php
@@ -18,22 +18,10 @@ class AuthApi
         try {
 
             if (!$user = JWTAuth::parseToken()->authenticate()) {
-                return response()->json(['user_not_found'], 404);
+                return response()->json(['not authorized'], 403);
             }
     
-        } /*catch (Tymon\JWTAuth\Exceptions\TokenExpiredException $e) {
-    
-            return response()->json(['token_expired'], $e->getStatusCode());
-    
-        } catch (Tymon\JWTAuth\Exceptions\TokenInvalidException $e) {
-    
-            return response()->json(['token_invalid'], $e->getStatusCode());
-    
-        } catch (Tymon\JWTAuth\Exceptions\JWTException $e) {
-    
-            return response()->json(['token_absent'], $e->getStatusCode());
-    
-        } */
+        } 
         catch(\Exception $e){
             return response()->json(['error' => 'Unauthorized'], 401);
         }
